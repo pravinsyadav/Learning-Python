4c8cce31b4a77236641f7f03cb96ce15f8193ff5
pravinsyadav <pravinsy2003@gmail.com>
2025-01-07 12:05:58 +0530

    added basic of pyhon folder

---
+++ Basics/Syntax.py
@@ -0,0 +1,9 @@
+print("hello pravin!")         #'print' is used for printing
+
+
+import sys
+print(sys.version)       #to see version of python we need to import sys module
+
+
+if(5>2):
+    print("5 is greater than 2")     #indentation is very important in python 
