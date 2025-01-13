8ca19172789b24368a8886cddb91fecc554053f3
pravinsyadav <pravinsy2003@gmail.com>
2025-01-13 19:27:37 +0530

    added conversion file

---
+++ Chapter 2/input.py
@@ -0,0 +1,7 @@
+a = input("enter no 1: ")
+b = input("enter no 2: ")
+print(a+b)     #here the output is 12 because input is taking as a string
+
+x = int(input("enter the no 1: "))
+y = int(input("enter the no : "))
+print(x+y)        #here the output is 3 because we converted string to int
---
+++ Chapter 2/integer_conversion.py
@@ -0,0 +1,9 @@
+a = "33.3"       # this is str
+print(type(a))
+
+b = float(a)       # here we converted str to float 
+print(type(b))
+
+
+x = int(float(a))
+print(type(x))
