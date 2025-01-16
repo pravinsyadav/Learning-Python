5fc7b5c300fdaaa5f13451ad6a48e2a92d53dcda
pravinsyadav <pravinsy2003@gmail.com>
2025-01-16 19:52:41 +0530

    added chapter 5 ps

---
+++ Chapter 5/ps5.py
@@ -0,0 +1,10 @@
+# convert hindi to english using dictionary 
+
+words  = {
+    "madad" : "help",
+    "kursi" : "chair",
+    "darwaja" : "door"
+}
+
+word = input("enter the word: ")
+print(words[word])             # it print value corresponding to its key...... here words = key ,,, word = value
