e53e58f23cb7341d288dd6a35e5325e756438f15
pravinsyadav <pravinsy2003@gmail.com>
2025-01-18 15:03:29 +0530

    added chapter 6

---
+++ Chapter 6/conditionals.py
@@ -0,0 +1,12 @@
+# if, elif, else condition->
+
+a = int(input("enter your age: "))
+
+if(a > 18):
+    print("you are eligible to vote")
+
+else:
+    print("you are not eligible to vote")
+
+
+# else can be written in single but elif and else cannot be written in single
---
+++ Chapter 6/ps6.py
@@ -0,0 +1,11 @@
+# marks1 = int(input("enter marks: "))
+# marks2 = int(input("enter marks: "))
+# marks3 = int(input("enter marks: "))
+
+# total_percentage = ((100) *(marks1 + marks2 + marks3))/300
+
+# if(total_percentage >= 40 and marks1 > 33 and marks2 > 33 and marks3 > 33):
+#     print("passed: ",total_percentage)
+# else:
+#     print("failed:",total_percentage)
+
