020e6e8d86ad2b91491a1083ca4d6f86a4eb8dd2
pravinsyadav <pravinsy2003@gmail.com>
2025-01-21 16:11:36 +0530

    added recursion file

---
+++ Chapter 8/recursion.py
@@ -0,0 +1,21 @@
+#RECURSION -> 'recursion' is a function which call itself
+
+'''
+factorial(1) = 1
+factorial(2) = 2 x 1
+factorial(3) = 3 x 2 x 1
+factorial(4) = 4 x 3 x 2 x 1
+factorial(5) = 5 x 4 x 3 x 2 x 1
+
+factorial(n) = n x (n-1) x ....... 3 x 2 x 1
+factorial(n) = n X factorial (n-1)
+'''
+
+def factorial(n):
+    if(n == 1 or n==0):
+        return 1
+    return n * factorial(n-1)
+
+
+n = int(input("enter a number: "))
+print(f"the factorial of number is {factorial(n)}")
