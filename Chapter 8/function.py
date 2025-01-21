b03de01bb6fade9a9bc3909cc3762127ff08709d
pravinsyadav <pravinsy2003@gmail.com>
2025-01-21 15:57:01 +0530

    added function file

---
+++ Chapter 8/function.py
@@ -0,0 +1,23 @@
+#FUNCTION -> 'function' is group of statements performing a specific task
+
+
+#Function Definition
+def avg():                
+    a = int(input("enter 1st no: "))
+    b = int(input("enter 2nd no: "))
+    c = int(input("enter 3rd no: "))
+
+    average = (a + b + c)/3
+    print(average)
+
+#Function Call
+avg()
+
+
+# there are two types of functions in python
+#         1. built in function
+#                Ex. len(), print(), range()
+#         2. user defined function
+#                Ex. avg(), func()
+
+
---
+++ Chapter 8/function_with_arguments.py
@@ -0,0 +1,4 @@
+def goodday(name):            # 'name' is parameter
+    print("good morning " + name)
+
+goodday("pravin")        # 'pravin' is argument       
