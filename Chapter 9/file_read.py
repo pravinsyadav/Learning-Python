b4ab42b5aa05a071ea1a183a4e69545e8a5da7bc
pravinsyadav <pravinsy2003@gmail.com>
2025-01-22 14:42:45 +0530

    added new file

---
+++ Chapter 9/file_read.py
@@ -0,0 +1,8 @@
+
+f = open("Learning-Python/Chapter 9/file.txt")             # here the file is by default on read mode if you want to write you need to add "w" in it
+data = f.read()
+print(data)
+f.close()
+
+
+
---
+++ Chapter 9/file_write.py
@@ -0,0 +1,6 @@
+st = "pravin is studying in ycce"
+
+
+f = open("Learning-Python/Chapter 9/myFile.txt", "w")  
+data = f.write(st)
+f.close
