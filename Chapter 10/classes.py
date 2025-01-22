0ceda9c99b879bbc4713a27adb6ac0e3c5ff4aeb
pravinsyadav <pravinsy2003@gmail.com>
2025-01-22 22:35:29 +0530

    added chapter 10

---
+++ Chapter 10/classes.py
@@ -0,0 +1,19 @@
+# creation of class 
+
+class employee:
+    name = "pravin"            # this is class attribute
+    language = "py"
+    salary = 50000
+
+
+# creation of object
+
+harry = employee()
+harry.name = "vivek"           # this is instance attribute
+print(harry.name, harry.language, harry.salary)
+
+
+# the instace attribute has higher preference than class attrubutes
+
+
+
