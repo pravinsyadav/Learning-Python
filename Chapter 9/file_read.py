d70e2d0eef6593f8e262d9e36e9906c078ec1d9a
Pravin Yadav <pravinsy2003@gmail.com>
2026-03-21 15:14:53 +0530

    updated files

--- Chapter 9/file_read.py
+++ Chapter 9/file_read.py
@@ -1,6 +1,6 @@
 
 f = open("Learning-Python/Chapter 9/file.txt")             # here the file is by default on read mode if you want to write you need to add "w" in it
-data = f.read()
+data = f.readlines()
 print(data)
 f.close()
 
--- Chapter 9/file_write.py
+++ Chapter 9/file_write.py
@@ -2,5 +2,6 @@ st = "pravin is studying in ycce"
 
 
 f = open("Learning-Python/Chapter 9/myFile.txt", "w")  
-data = f.write(st)
-f.close
+data = f.write(st)             #it will overwrite old data 
+data = f.write("\nhello")     #it will add new line without overwriting old data 
+f.close()
