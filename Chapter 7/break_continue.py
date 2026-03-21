3cfe7e969f7c5a92b6777ab233ecdf172bd5d271
Pravin Yadav <pravinsy2003@gmail.com>
2026-03-21 12:10:49 +0530

    updated file

--- Chapter 7/break_continue.py
+++ Chapter 7/break_continue.py
@@ -16,3 +16,10 @@ for i in range(25):
     if(i == 12):
         continue         # it will skip 12th iteration
     print(i)        # this will print from 0 to 24 but it does not print 12 
+
+
+for i in range(25):
+    print(i,end=" ")       # end means don't go to next line ... print space instead 
+
+print()               # this will print empty line or it moves cursor to next line 
+print(*range(25))            # this will print range from 0 to 24 
