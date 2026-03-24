2a0d33409ada6329d4ab7745e0cc25f424288859
Pravin Yadav <pravinsy2003@gmail.com>
2026-03-24 14:50:22 +0530

    modified datatype file

--- Chapter 2/datatype.py
+++ Chapter 2/datatype.py
@@ -1,5 +1,12 @@
-x = 5        #integer
-x = "pravin"   #string
-x = 3.333      #float
-x = None      #none type variable
-x = False      # boolean
+a = 5        #integer
+b = "pravin"   #string
+c = 3.333      #float
+d = None      #none type variable
+e = False      # boolean
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
